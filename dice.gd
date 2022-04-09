8f41ead6176ae25af2e44677178640d1b34b1cb7
Dexter Liu <dex@Dexters-MacBook-Pro.local>
2022-04-09 15:18:10 -0700

    Go left and right to do hover

--- dice.gd
+++ dice.gd
@@ -9,18 +9,38 @@ extends Node2D
 var dice_value = 1
 var rng = RandomNumberGenerator.new()
 
+var is_held = false
+var hover = false
+
 # Called when the node enters the scene tree for the first time.
 func roll_dice():
 	dice_value = rng.randi_range(1,6)
 	$dice_label.set_text(str(dice_value))
 
+func toggle():
+	if(is_held == true):
+		is_held = false
+	else:
+		is_held = true
+
+	if(is_held == true):
+		$selected.visible = true
+	else:
+		$selected.visible = false
+
 func _ready():
 	rng.randomize()
 	roll_dice()
+	$selected.visible = false
+	$hover.visible = false
 	pass # Replace with function body.
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if(hover == false):
+		$hover.visible = false
+	else:
+		$hover.visible = true
+	pass
--- main.gd
+++ main.gd
@@ -8,6 +8,20 @@ extends Node2D
 export(PackedScene) var dice
 export(PackedScene) var cash_in
 
+# dice position index is current position of your cursor, goes from 1-6
+var dice_position_index = 0
+var dice_array = []
+
+# this is our little toggle so that hitting right doesn't +71 dice_position_index within 3 milliseconds
+var input_ok
+
+func handle_hover():
+
+	for i in 6:
+		dice_array[i].hover = false
+	
+	dice_array[dice_position_index].hover = true
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +29,7 @@ func _ready():
 	for i in 6:
 		var dice_instance = dice.instance()
 		dice_instance.position = Vector2(300+100*i, 300)
+		dice_array.append(dice_instance)
 		add_child(dice_instance)
 	
 	# sorry this ist stupid at first
@@ -39,10 +54,25 @@ func _ready():
 	add_child(cash_in_instance_2)
 	add_child(cash_in_instance_3)
 	add_child(cash_in_instance_4)
+	handle_hover()
 	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	if(Input.is_action_pressed("ui_right") and input_ok == true and dice_position_index < 5):
+		dice_position_index += 1
+		handle_hover()
+		input_ok = false
+
+	if(Input.is_action_pressed("ui_left") and input_ok == true and dice_position_index > 0):
+		dice_position_index -= 1
+		handle_hover()
+		input_ok = false
+		
+	if(!(Input.is_action_pressed("ui_right")) and !(Input.is_action_pressed("ui_left"))):
+		input_ok = true
+		
+	pass
